6c439173f4ec83cdce96edb3c087d8af8210e228
John Steele <33888807+camusicjunkie@users.noreply.github.com>
2022-12-03 19:21:09 +0100

    moved hash to new file

--- 2022/02/Part1.ps1
+++ 2022/02/Part1.ps1
@@ -1,19 +1,5 @@
 $value = [System.Collections.Generic.List[int]] @()
-
-$choices = @{
-    a = 1
-    b = 2
-    c = 3
-    x = 1
-    y = 2
-    z = 3
-}
-
-$winner = @{
-    1 = 3
-    3 = 2
-    2 = 1
-}
+. .\hashes.ps1
 
 foreach ($item in (Get-Content .\input.txt)) {
     $you, $me = -split $item
---
+++ 2022/02/hashes.ps1
@@ -0,0 +1,24 @@
+# rock = 1
+# paper = 2
+# scissors = 3
+
+$choices = @{
+    a = 1
+    b = 2
+    c = 3
+    x = 1
+    y = 2
+    z = 3
+}
+
+$winner = @{
+    1 = 3
+    3 = 2
+    2 = 1
+}
+
+$loser = @{
+    3 = 1
+    2 = 3
+    1 = 2
+}
